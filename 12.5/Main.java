3e56b10f30d38278aba7014701982c52a731ad77
dventura11 <dventuras11@gmail.com>
2017-03-02 04:02:33 -0600

    12.5 binary search solution added

--- 12.5/Main.java
+++ 12.5/Main.java
@@ -1,5 +1,7 @@
 public class Main {
 
+    private static final int REPEAT = 100000;
+
     public static void main(String[] args) {
         int[][] cases = {
             {1,1},
@@ -10,18 +12,55 @@ public class Main {
             {Integer.MAX_VALUE,46340},
         };
 
-        for (int[] arr : cases) {
-            int result = maxSquareRoot(arr[0]);
-            System.out.printf("Verifing %d...\n", arr[0]);
-            if (result != arr[1]) {
-                System.out.printf("expected: %d and %d was returned\n",  arr[1],  result);
+        long since = System.currentTimeMillis();
+        for (int i = 0; i < REPEAT; i++) {
+            for (int[] arr : cases) {
+                int result = maxSquareRoot(arr[0]);
+                if (result != arr[1]) {
+                    System.out.printf("%d was verified and %d  was expected but %d was returned\n", arr[0], arr[1],  result);
+                }
             }
         }
+        System.out.println(System.currentTimeMillis() - since);
+
+        since = System.currentTimeMillis();
+        for (int i = 0; i < REPEAT; i++) {
+            for (int[] arr : cases) {
+                int result = maxSquareRootBinarySearch(arr[0]);
+                if (result != arr[1]) {
+                    System.out.printf("%d was verified and %d  was expected but %d was returned\n", arr[0], arr[1],  result);
+                }
+            }
+        }
+        System.out.println(System.currentTimeMillis() - since);
+
+
     }
 
+    //raiz de n + 1
     public static int maxSquareRoot(int n) {
         long i;
         for (i = 0; (i*i) <= n; i++);
         return (int)i-1;
     }
+
+    //log n
+    public static int maxSquareRootBinarySearch(int n) {
+        long left = 0, right = n;
+        long middle, square;
+        while (left <= right) {
+            middle = left + ((right - left) / 2);
+            square = middle * middle;
+            if (square == n) {
+                return (int) middle;
+            }
+            if (square < n) {
+                left = middle + 1;
+            } else {
+                right = middle -1;
+            }
+        }
+        return (int)left-1;
+    }
+
 }
